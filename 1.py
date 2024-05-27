5b37c953ee37fb13f0987c095fbcabed87084744
Junming1120 <159005069+Junming1120@users.noreply.github.com>
2024-05-27 12:25:59 -0700

    1

---
+++ 1.py
@@ -0,0 +1,15 @@
+import tkinter as tk 
+from tkinter import *
+from tkinter import ttk 
+window = tk.Tk()
+entry1 = tk.Entry(window,text="Entry widgets can be typed in", width=10)
+lable2 = tk.Label(window, text="x")
+entry3 = tk.Entry(window,text="Entry widgets can be typed in", width=10)
+button4 = tk.Button(window,text="=")
+entry5 = tk.Entry(window,text="Entry widgets can be typed in", width=20)
+entry1.pack(side=LEFT)
+lable2.pack(side=LEFT)
+entry3.pack(side=LEFT)
+button4.pack(side=LEFT)
+entry5 .pack(side=LEFT)
+window.mainloop()
